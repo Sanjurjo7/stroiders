515fcbd33864fdca9a081c4e9db61f1bc76299e0
Sanjurjo Family <Sanjurjo2@Sanjurjos-MacBook-Air.local>
2016-12-11 16:32:46 -0500

    Updated the character class sightly, mostly testing git at this point.

--- Character.py
+++ Character.py
@@ -1,9 +1,16 @@
 class Character (object):
     """ This is the abstract character class which implements some of the primary features of the character like inventory, skills, and personality."""
     inventory = []
+    skills = []
+
     def __init__(self):
         self.inventory
+    def invMan(self):
         # TODO: Add inventory management.
+        # Discussion: Should the character have multiple slots here, determined by their gear/suit. Would be thematically appropriate.
+
+    def skillMan(self):
         # TODO: Add skills management.
+        
         # TODO: Add Decision Tree algorithms.
         # TODO: Add Pathfinding
