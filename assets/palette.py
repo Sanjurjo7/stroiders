f021f78703bdf90dde19abe9ceac781226b9f7e9
Technology Teacher <techteacher@NCSBRANDON.Local>
2016-12-13 12:34:51 -0500

    Added downloadable python file for the palette for the game.

---
+++ assets/palette.py
@@ -0,0 +1,19 @@
+# Generated with GIMP Palette Export
+# Based on the palette 16Color - DawnBringer's
+colors={
+'Index0': '#140c1c',
+'Index1': '#442434',
+'Index2': '#30346d',
+'Index3': '#4e4a4e',
+'Index4': '#854c30',
+'Index5': '#346524',
+'Index6': '#d04648',
+'Index7': '#757161',
+'Index8': '#597dce',
+'Index9': '#d27d2c',
+'Index10': '#8595a1',
+'Index11': '#6daa2c',
+'Index12': '#d2aa99',
+'Index13': '#6dc2ca',
+'Index14': '#dad45e',
+'Index15': '#deeed6'}
