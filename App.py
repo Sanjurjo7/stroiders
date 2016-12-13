6d40247d17ff57ba4f29d32e415bb433a15f3703
Sanjurjo Family <Sanjurjo2@Sanjurjos-MacBook-Air.local>
2016-12-13 07:08:05 -0500

    Made an initial trial on blitting images to screen.

--- App.py
+++ App.py
@@ -1,16 +1,25 @@
 import pygame
 from pygame.locals import*
+from Stage import Stage
 
 class App:
+    bg = 0
+    tileImage = 0
+
     def __init__(self):
         self._running = True
-        self._display_surf = None
+        self.screen = None
         self.size = self.width, self.height = 640, 400
+        self.stage = Stage(64,40)
+        self.scale = 10
 
     def on_init(self):
         pygame.init()
-        self._display_surf = pygame.display.set_mode(self.size, pygame.HWSURFACE | pygame.DOUBLEBUF)
+        self.screen = pygame.display.set_mode(self.size, pygame.HWSURFACE | pygame.DOUBLEBUF)
         self._running = True
+        self.bg = pygame.image.load('assets/starsBKG.png')
+        self.tileImage = pygame.image.load('assets/tileSample.png')
+        
 
     def on_event(self, event):
         if event.type == pygame.QUIT:
@@ -19,7 +28,11 @@ class App:
     def on_loop(self):
         pass
     def on_render(self):
-        pass
+        self.screen.fill((0,0,0))
+        self.screen.blit(self.bg, (0,0))
+        for tile in self.stage.stage:
+            self.screen.blit(self.tileImage, tile.getTileLocation()*self.scale)
+        pygame.display.flip()
     def on_cleanup(self):
         pygame.quit()
 
