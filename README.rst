6846bd4a850f1368ccf20518019c4e64a969f921
Thomas Sanjurjo <sanjurjo7@gmail.com>
2017-06-26 06:58:53 -0400

    Finalized structure

---
+++ README.rst
@@ -0,0 +1,14 @@
+Stroiders: A Zero G Diggy Game
+==============================
+
+Stroiders will be a mining game similar to DigDug but with zero g elements and hazzards
+
+Elements to consider:
+Gravity
+Vacuum
+Hazards
+Materials
+Economy
+Power
+
+Currently writing the program in Python3 and pygame.
--- setup.py
+++ setup.py
@@ -1,13 +1,23 @@
 from setuptools import setup
 
-setup(name='stroiders',
-        version='0.1',
-        description='zero grav diggy game',
-        url='https://github.com/Sanjurjo7/stroiders',
-        author='Sanjurjo7',
-        author_email='sanjurjo7@gmail.com',
-        license='MIT',
-        packages=['stroiders'],
-        zip_safe=False)
+with open ('README.rst') as f:
+    readme = f.read()
+
+with open('LICENSE') as f:
+    license = f.read()
+
+
+setup(
+    name='stroiders',
+    version='0.1.0',
+    description='zero grav diggy game',
+    url='https://github.com/Sanjurjo7/stroiders',
+    long_description=readme,
+    author='Thomas Sanjurjo',
+    author_email='sanjurjo7@gmail.com',
+    license=license,
+    packages=find_packages(exclude=('tests', 'docs')),
+    zip_safe=False
+)
 
 
